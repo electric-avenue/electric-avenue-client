b4d22de73f193aefcec73f8d8bd9bec62e355b36
richardavendano <avendano.richard@gmail.com>
2014-11-17 11:45:38 -0800

    removed commented code

--- www/views/auth/auth.js
+++ www/views/auth/auth.js
@@ -53,35 +53,3 @@ angular.module('authView', ['auth'])
       });
   };
 });
-
-// /**
-//  * Login Controller
-//  * @module LoginCtrl
-//  */
-// angular.module('starter.login', ['auth'])
-// .controller('LoginCtrl', function($scope, $timeout, Auth, $state) {
-//   // Form data for the login modal
-//   // Perform the login action when the user submits the login form
-//   $scope.doLogin = function() {
-//     // Set defaults
-//     var user = $scope.loginData.username || undefined;
-//     var pass = $scope.loginData.password || undefined;
-//     // Check to see if all data exists
-//     if (!user || !pass) {
-//       // It does not. Give the user an error
-//       $scope.loginError = true;
-//       return;
-//     }
-//     // Login the user
-//     Auth.login(user, pass)
-//       .then(function(data) {
-//         if (!data) {
-//           $scope.loginError = true;
-//         } else {
-//           $state.transitionTo('app.home');
-//         }
-//       }).catch(function(err) {
-//         $scope.loginError = true;
-//       });
-//   };
-// })
