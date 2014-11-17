aa7e80c87603868b13158557ca803bfdb733eccb
Rishi Goomar <rishig92@gmail.com>
2014-11-17 12:22:07 -0800

    added vendormodal

--- www/views/user/dashboard/dashboard.js
+++ www/views/user/dashboard/dashboard.js
@@ -1,4 +1,19 @@
 angular.module('userDashboard', [])
-.controller('UserDashboardCtrl', function($scope) {
-  
+.controller('UserDashboardCtrl', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('views/user/vendorprofile/vendorprofile.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.vendorModal = modal;
+  });
+  $scope.showVendor = function() {
+    $scope.vendorModal.show();
+  };
+  $scope.hideVendor = function() {
+    $scope.vendorModal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.vendorModal.remove();
+  });
 });
