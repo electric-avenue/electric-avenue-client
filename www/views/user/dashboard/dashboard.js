8975b0d8278a35ca234fbe028da589ea7193e4a7
jamondouglas <terrildouglas@gmail.com>
2014-11-18 20:30:40 -0800

    Use cordova to grab geo location

--- www/views/user/dashboard/dashboard.js
+++ www/views/user/dashboard/dashboard.js
@@ -1,5 +1,5 @@
-angular.module('userDashboard', ['search', 'leaflet-directive'])
-.controller('UserDashboardCtrl', function($scope, $ionicModal, Search, $state) {
+angular.module('userDashboard', ['search', 'leaflet-directive','ngCordova'])
+.controller('UserDashboardCtrl', function($scope, $ionicModal, Search, $state,$cordovaGeolocation) {
   $scope.data = {
     vendors: [],
     selected: {}
@@ -58,16 +58,51 @@ angular.module('userDashboard', ['search', 'leaflet-directive'])
   $scope.selectedFilters = _.filter($scope.filters, 'selected');
   // console.log(JSON.stringify($scope.selectedFilters));
 
+// MAP Implementation JAMON'S Stuff//
+  $scope.controls = {
+    custom: []
+  };
+  
+  var MyControl = L.control();
+  MyControl.setPosition('bottomright');
+  MyControl.onAdd = function () {
+    var className = 'icon ion-compass mycompass', 
+    container = L.DomUtil.create('div', className);
+    return container;
+  }
+  $scope.controls.custom.push(MyControl);
+
+
+  var locate = function(){
+    $cordovaGeolocation
+    .getCurrentPosition()
+    .then(function(position){
+      $scope.map.center.lat = position.coords.latitude;
+      $scope.map.center.lng = position.coords.longitude;
+      $scope.map.center.zoom = 16;
+
+      $scope.map.markers['now'] = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        message: " This is you!",
+        focus: true,
+        draggable: false
+      }, function(err){
+        console.log("Error getting your location.",err);
+      }
+    });
+  };
   $scope.map = {
     defaults: {
       tileLayer: "https://{s}.tiles.mapbox.com/v3/deziak1906.k8mphke2/{z}/{x}/{y}.png",
       maxZoom: 18,
-      zoomControlPosition: 'bottomleft'
+      zoomControlPosition: 'bottomleft',
+      attributionControl:false
     },
     center: {
-      lat: 43.661165,
-      lng: -79.390919,
-      zoom: 15
+      // lat: 43.661165,
+      // lng: -79.390919,
+      // zoom: 15
     },
     markers : {},
     events: {
@@ -75,6 +110,13 @@ angular.module('userDashboard', ['search', 'leaflet-directive'])
         enable: ['context'],
         logic: 'emit'
       }
+    } 
+  };
+  locate();
+
+  $scope.goTo = function(location){
+    if(location === 0){
+      $scope.locate();
     }
   };
 });
