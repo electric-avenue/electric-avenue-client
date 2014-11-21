03a70c453a8df890e370563ce0472d4e8a880ae5
richardavendano <avendano.richard@gmail.com>
2014-11-20 16:35:54 -0800

    removed old code

--- www/services/vendorprofile.js
+++ www/services/vendorprofile.js
@@ -26,10 +26,7 @@ angular.module('vendorFactory', ['angularFileUpload', 'auth'])
   var getStatus = function(vendor, callback) {
     return $http({
       method: 'GET',
-      url: config.baseUrl + '/api/vendor/' + vendor + '/status'//,
-      // data: {
-      //   vendor: vendor
-      // }
+      url: config.baseUrl + '/api/vendor/' + vendor + '/status'
     })
     .then(function(res) {
       console.log('Success:', res);
